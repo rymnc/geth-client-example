ff3be95a80ba0d1409e997a39fa789b46b99feed
rymnc <aaryamannchallani7@gmail.com>
2021-10-13 11:31:40 +0530

    feat: init

---
+++ go.mod
@@ -0,0 +1,18 @@
+module test.com/mod
+
+go 1.16
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
+	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/ethereum/go-ethereum v1.10.9 // direct
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/kubemq-io/kubemq-go v1.7.2 // indirect
+	github.com/rs/zerolog v1.25.0 // indirect
+	github.com/shirou/gopsutil v3.21.9+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.9 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
+)
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/kubemq-io/kubemq-go"
+)
+
+type FuncKubeMqClient func() *kubemq.EventsClient
+
+func getKubeMqClient(ctx context.Context) FuncKubeMqClient {
+	client, err := kubemq.NewEventsClient(
+		ctx,
+		kubemq.WithAddress("localhost", 50000),
+		kubemq.WithClientId("client_id"),
+		kubemq.WithTransportType(kubemq.TransportTypeGRPC),
+		kubemq.WithCheckConnection(true),
+	)
+	if err != nil {
+		log.Fatal().Msg(err.Error())
+	}
+	return func() *kubemq.EventsClient {
+		return client
+	}
+}
+
+func publish(ctx context.Context, pubClient *kubemq.EventsClient, cleanedBlock *CleanedBlock) {
+	jsonified, err := json.Marshal(cleanedBlock)
+	if err != nil {
+		log.Fatal().Msg(err.Error())
+	}
+	errPub := pubClient.Send(
+		ctx,
+		kubemq.NewEvent().SetChannel("newBlocks").SetBody(jsonified),
+	)
+	if errPub != nil {
+		log.Fatal().Msg(errPub.Error())
+	}
+}
+
+type CleanedBlock struct {
+	Hash         string
+	Number       uint64
+	Time         uint64
+	Transactions int
+}
+
+func formatBlock(block *types.Block) CleanedBlock {
+	cleanedBlock := CleanedBlock{
+		Hash:         block.Hash().Hex(),
+		Number:       block.Number().Uint64(),
+		Time:         block.Time(),
+		Transactions: len(block.Transactions()),
+	}
+	return cleanedBlock
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal().Msg("Error loading .env file")
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	client, err := ethclient.Dial(os.Getenv("RPC_URL"))
+	if err != nil {
+		log.Fatal().Msg(err.Error())
+	}
+	log.Info().Msg("Connected to Alchemy")
+
+	pubClient := getKubeMqClient(ctx)()
+	log.Info().Msg("Connected to KubeMQ")
+
+	headers := make(chan *types.Header)
+	sub, err := client.SubscribeNewHead(context.Background(), headers)
+	if err != nil {
+		log.Fatal().Msg(err.Error())
+	}
+	log.Info().Msg("Subscribed to new headers")
+
+	for {
+		select {
+		case err := <-sub.Err():
+			log.Fatal().Msg(err.Error())
+		case header := <-headers:
+			log.Info().Msg("Got new block")
+			block, err := client.BlockByHash(context.Background(), header.Hash())
+			if err != nil {
+				log.Fatal().Msg(err.Error())
+			}
+			formattedBlock := formatBlock(block)
+			go publish(ctx, pubClient, &formattedBlock)
+		}
+	}
+}
